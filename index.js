b1d203107a377a85b33bf6ef3dd49f7e54445ee9
sebokdas1 <sebok.das.ff@gmail.com>
2022-05-03 09:33:14 +0600

    load single and multiple data from mongodb

--- index.js
+++ index.js
@@ -2,30 +2,52 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('server running');
-});
-
-
-
 
 
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.zfupy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('db connected')
-    // perform actions on the collection object
-    client.close();
+
+async function run() {
+    try {
+        await client.connect();
+        const itemCollection = client.db("nutrioWarehouse").collection("item");
+
+        //Find multiple data
+        app.get('/item', async (req, res) => {
+            const query = {};
+            const cursor = itemCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        });
+
+        //find single data
+        app.get('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const item = await itemCollection.findOne(query);
+            res.send(item);
+        });
+
+
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+app.get('/', (req, res) => {
+    res.send('server running');
 });
 
-
+run().catch(console.dir)
 app.listen(port, () => {
     console.log('crd running', port);
 });
